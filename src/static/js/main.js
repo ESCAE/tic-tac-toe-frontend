0b44ed166e40fe54e5c5e6bf5c3309244e28ba27
W. Ely Paysinger <paysinger@gmail.com>
2017-07-25 13:35:31 -0700

    working on nav bar.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -2,7 +2,6 @@ $('.square').click(function(){
   if ($(this).text() != 'X' && $(this).text() != 'O') {
     let move = $('.square').index($(this));
     let board = $('.square').text();
-    // debugger;
     $.ajax({
       url: "http://ec2-50-112-145-55.us-west-2.compute.amazonaws.com/api/v1/bot/",
       type: 'POST',
@@ -10,29 +9,7 @@ $('.square').click(function(){
       contentType: 'application/json',
       data: JSON.stringify({board: board, move: move}),
       success: function ( data ) {
-        $('.square').each( function ( idx ){
-          $(this).text(data['board'].charAt(idx));
-        });
-        if(data['WL'] === true) {
-          data['Wline'].forEach(function(index){
-            $('.square').eq(index).css("color", "red");
-          });
-          alert('You Win!');
-        } else if ( data['WL'] === false ){
-          setTimeout(function(){
-            $('.square').eq(data['move']).text('O');
-            data['Wline'].forEach(function(index){
-              $('.square').eq(index).css("color", "red");
-            });
-          }, 800);
-          setTimeout(function(){
-            alert('You Lose!');
-          }, 900);
-        } else {
-          setTimeout(function(){
-            $('.square').eq(data['move']).text('O')
-          }, 800);
-        }
+        renderMove(data);
       },
       error: function(){
         console.log("Cannot get data");
@@ -41,6 +18,29 @@ $('.square').click(function(){
   }
 });
 
-function renderBoard(data) {
-  $squares = $
+function renderMove(data) {
+  $('.square').each( function ( idx ){
+    $(this).text(data['board'].charAt(idx));
+  });
+  if(data['WL'] === true) {
+    data['Wline'].forEach(function(index){
+      $('.square').eq(index).css("color", "green");
+    });
+    alert('You Win!');
+  } else if ( data['WL'] === false ){
+    console.log(data['Wline']);
+    setTimeout(function(){
+      $('.square').eq(data['move']).text('O');
+      data['Wline'].forEach(function(index){
+        $('.square').eq(index).css("color", "red");
+      });
+    }, 800);
+    setTimeout(function(){
+      alert('You Lose!');
+    }, 900);
+  } else {
+    setTimeout(function(){
+      $('.square').eq(data['move']).text('O')
+    }, 800);
+  }
 }
